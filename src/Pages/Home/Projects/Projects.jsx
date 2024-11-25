5f49ebbda8b47b1cd63c0af66ff4d183afb25ae9
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-11-25 15:20:12 +0600

    design update

--- src/Pages/Home/Projects/Projects.jsx
+++ src/Pages/Home/Projects/Projects.jsx
@@ -1,5 +1,5 @@
 import HeadingTitle from "../../HeadingTitle/HeadingTitle";
-import photogrphy from '../../../assets/summer-camp.png';
+import photography from '../../../assets/summer-camp.png';
 import toy from '../../../assets/toy.png';
 import chef from '../../../assets/kitchen.png';
 import './Projects.css';
@@ -9,7 +9,7 @@ import { FaGithub, FaLink } from "react-icons/fa";
 const Projects = () => {
     const projects = [
         {
-            image: photogrphy,
+            image: photography,
             name: 'Photo Camp',
             client: 'https://github.com/Ashrafulhiderjayed/summer-photo-cam-client',
             server: 'https://github.com/Ashrafulhiderjayed/summer-photo-camp-server',
@@ -32,32 +32,32 @@ const Projects = () => {
             live: 'https://the-kitchen-15a59.web.app/',
             feature: 'Here I implemented firebase authentication system including private route'
         },
-     ]
+    ]
     return (
         <>
-        <HeadingTitle text="RECENT PROJECTS"/>
-        
-        <div className="grid md:grid-cols-3 gap-8 text-slate-300" id="skills">
-            {
-                projects.map((project, index) => <div key={index} className="shadow-lg ">
+            <HeadingTitle text="RECENT PROJECTS" />
 
-                <figure><img className="fullimg h-72 xl:h-80" src={project.image} alt="project-image"/></figure>
-                
-                <div className="card w-96 glass text-justify">
-                    <div className="card-body">
-                    <h2 className="card-title text-slate-200">Features</h2>
-                        <p>{project.feature}</p>
-                        <div className="card-actions justify-between">
-                        <button className="btn btn-primary flex-col justify-center"><FaGithub /><a href={project.client}><h4>Client</h4></a></button>
-                        <button className="btn btn-primary flex-col justify-center"><FaGithub  /><a href={project.server}><h4>Server</h4></a></button>
-                        <button className="btn btn-primary flex-col justify-center"><FaLink /><a href={project.live}><h4>Website</h4></a></button>
+            <div className="grid md:grid-cols-3 gap-8 text-slate-300" id="skills">
+                {
+                    projects.map((project, index) => <div key={index} className="shadow-lg ">
+
+                        <figure><img className="fullimg h-72 xl:h-80 w-96" src={project.image} alt="project-image" /></figure>
+
+                        <div className="card glass text-justify">
+                            <div className="card-body">
+                                <h2 className="card-title text-slate-200">Features</h2>
+                                <p>{project.feature}</p>
+                                <div className="card-actions justify-between">
+                                    <button className="btn btn-primary flex-col justify-center"><FaGithub /><a href={project.client}><h4>Client</h4></a></button>
+                                    <button className="btn btn-primary flex-col justify-center"><FaGithub /><a href={project.server}><h4>Server</h4></a></button>
+                                    <button className="btn btn-primary flex-col justify-center"><FaLink /><a href={project.live}><h4>Website</h4></a></button>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
 
-                </div>)
-            }
-        </div>
+                    </div>)
+                }
+            </div>
         </>
     );
 };
